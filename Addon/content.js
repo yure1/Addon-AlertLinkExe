7608d2e232158f8f314b55e13c72794924a322d3
yure1 <babieru.t@gmail.com>
2019-07-15 17:30:41 +0900

    Add content.js and manifest.json

---
+++ Addon/content.js
@@ -0,0 +1,35 @@
+function changeLinkColor(exelink){
+    exelink.style.color = '#ffffff';
+    exelink.style.backgroundColor = '#ff0000';
+}
+
+function checkExeLink() {
+    let aTag = document.getElementsByTagName('a');
+
+    for(let i = 0; aTag.length; i++) {
+
+        if(aTag[i].getAttribute('href').toLowerCase().indexOf(".exe") !== -1) {
+        
+            changeLinkColor(aTag[i]);
+        
+        }
+    }
+}
+
+function ManualCheckExeLink() {
+
+    document.addEventListener("click", function(evt) {
+        let shiftClick = evt.shiftKey;
+    
+        if(shiftClick === true) {
+        
+            checkExeLink();
+        
+        }
+    
+    })
+
+}
+
+window.addEventListener("DOMContentLoaded", ManualCheckExeLink());
+window.addEventListener("DOMContentLoaded", checkExeLink());
